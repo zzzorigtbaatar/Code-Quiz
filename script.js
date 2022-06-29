e1c1f41b88aba9af2c5347d3230d72d91187ac29
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-06-28 17:26:48 -0700

    add js variables

---
+++ script.js
@@ -0,0 +1,20 @@
+//elements on the document
+var welcomeText = document.querySelector(".welcome-text");
+var score = document.querySelector(".score")
+var startButton = document.querySelector(".start-button");
+var resetButton = document.querySelector(".reset-button");
+var timerElement = document.querySelector(".timer-count");
+var gameOver = false;
+
+//initial values for variables
+var scoreCounter = 0;
+var timer;
+var timerCount = 0;
+
+//runs when the page is loaded
+function init() {
+
+}
+
+//call init() when page script is run
+init();
